96c7ef85bcd457ba053bf4882dae2efbdd3e51e5
MissionBitChristopher <55220393+MissionBitChristopher@users.noreply.github.com>
2019-12-08 14:43:43 -0800

    Add files via upload

---
+++ Assets/LevelManager copy.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log ("Level load requested for :" + name);
+		SceneManager.LoadScene (name);
+	}
+
+	public void Quitrequest(){
+		Debug.Log("I want to quit");
+		Application.Quit ();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/NumberWizard copy.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class NumberWizard : MonoBehaviour {
+	int max;
+	int min;
+	int guess;
+	int maxGuessesAllowed = 8;
+
+	public Text text;
+	void Start(){
+		StartGame ();
+	}
+	// Use this for initialization
+	public void StartGame(){
+		max = 1000;
+		min = 1;
+		ChangeGuess();
+
+	}
+	// Update is called once per frame
+
+	public void GuessHigher(){
+		min = guess;
+		ChangeGuess ();
+	}
+
+	public void GuessLower(){
+		max = guess;
+		ChangeGuess ();
+	}
+	public void ChangeGuess(){
+        //Use the line under to have a random number in the range
+        //guess = Random.Range(min, max + 1);
+        guess = (max + min) / 2;
+	   	text.text = guess.ToString() + "?";
+		maxGuessesAllowed = maxGuessesAllowed - 1;
+		if (maxGuessesAllowed <= 0) {
+            SceneManager.LoadScene("Win");
+		}
+	}
+	public void LoadLevel(){
+        SceneManager.LoadScene("Lose");
+	} 
+
+}
